895d0a9b661fee1c6f16165a1a2c977785f906fb
MatSzin <47425380+MatSzin@users.noreply.github.com>
2022-12-29 15:55:38 +0000

    hi

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Never Have I Ever AI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to the definitive AI for your Never Have I Ever Questionset" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
